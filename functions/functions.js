5dcb3e3efa2648416573a4fe0bfb665299eafe54
Asmit Singh <ashmitsingh068@gmail.com>
2023-12-30 17:09:41 +0530

    arrow-function and this-keword

---
+++ functions/functions.js
@@ -0,0 +1,30 @@
+const user = {
+  name: "asmit",
+  price: 999,
+
+  welcomeMessage: function () {
+    console.log(`${this.name}, welcome to website`)
+    console.log(this)
+  },
+}
+
+// user.welcomeMessage()
+// user.name = "akash"
+// user.welcomeMessage()
+
+// console.log(this) // global object is empty
+
+// function chai() {
+//   console.log(this)
+// }
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+const addTwo = (num1, num2) => num1 + num2 // implicit return
+
+const reutnObject = (a, b) => ({ username: "asmit" })
+console.log(addTwo(3, 4))
